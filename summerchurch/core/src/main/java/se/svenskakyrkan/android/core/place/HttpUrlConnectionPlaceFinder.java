c0625d97d7c6cd285eb9fe961f98c92762cd89f9
svkhao <henrik.arro@svenskakyrkan.se>
2015-02-23 17:00:55 +0100

    Added some missing Javadoc

--- summerchurch/core/src/main/java/se/svenskakyrkan/android/core/place/HttpUrlConnectionPlaceFinder.java
+++ summerchurch/core/src/main/java/se/svenskakyrkan/android/core/place/HttpUrlConnectionPlaceFinder.java
@@ -21,14 +21,23 @@ import java.util.Set;
 import jdk.nashorn.internal.parser.JSONParser;
 
 /**
- * Created by svkhao on 2015-02-11.
+ * An implementation of the {@link PlaceFinder} interface that uses an <code>HttpUrlConnection</code>
+ * to connect to the <a href="http://api.svenskakyrkan.se/platser/v3-latest/doc/">places API</a>.
+ *
+ * @author Henrik Arro
  */
 public class HttpUrlConnectionPlaceFinder implements PlaceFinder {
 
-    private static final String PLACES_URL = "http://api.svenskakyrkan.se/platser/v3/place";
+    /**
+     * The default URL to use to connect to the places API.
+     */
+    public static final String PLACES_URL = "http://api.svenskakyrkan.se/platser/v3/place";
 
+    /**
+     * The default API key to use to authenticate requests to the places API.
+     */
     // TODO: Replace this with your own API key.
-    private static final String API_KEY = "b95eeb81-65fe-49db-8029-a85234a2247a";
+    public static final String API_KEY = "b95eeb81-65fe-49db-8029-a85234a2247a";
 
     private static final byte[] BUFFER = new byte[1024];
 
@@ -36,6 +45,13 @@ public class HttpUrlConnectionPlaceFinder implements PlaceFinder {
     private String apiKey;
     private PlaceParser placeParser;
 
+    /**
+     * Creates a new place finder.
+     *
+     * @param baseUrl the base URL of the places API
+     * @param apiKey the API key to use to authenticate the requests
+     * @param placeParser the {@link PlaceParser} to use to parse the result
+     */
     public HttpUrlConnectionPlaceFinder(String baseUrl, String apiKey, PlaceParser placeParser) {
         if (baseUrl == null || apiKey == null || placeParser == null) {
             throw new IllegalArgumentException("Arguments must not be null: baseUrl=" + baseUrl
@@ -46,6 +62,11 @@ public class HttpUrlConnectionPlaceFinder implements PlaceFinder {
         this.placeParser = placeParser;
     }
 
+    /**
+     * Creates a new place finder with default settings.
+     *
+     * @return a new place finder using default settings
+     */
     public static HttpUrlConnectionPlaceFinder createDefault() {
         return new HttpUrlConnectionPlaceFinder(PLACES_URL, API_KEY, new SvkPlaceParser());
     }
--- summerchurch/core/src/main/java/se/svenskakyrkan/android/core/place/PlaceFinder.java
+++ summerchurch/core/src/main/java/se/svenskakyrkan/android/core/place/PlaceFinder.java
@@ -4,10 +4,24 @@ import java.io.IOException;
 import java.util.Set;
 
 /**
- * Created by svkhao on 2015-02-10.
+ * Defines ways to find {@link Place Places}.
+ *
+ * @author Henrik Arro
  */
 public interface PlaceFinder {
 
+    /**
+     * Finds places of the given type, within a maximum distance from a given point.
+     *
+     * @param placeType the type of place to find
+     * @param longitude the longitude of the point from which to search
+     * @param latitude the latitude of the point from which to search
+     * @param maxDistanceMeters the maximum distance to search, in meters
+     *
+     * @return a set of places matching the criteria
+     *
+     * @throws IOException if searching for the places failed
+     */
     Set<Place> find(PlaceType placeType, double longitude, double latitude, int maxDistanceMeters) throws IOException;
 
 }
